7e7456eb5acd5d7cf51491a47ff93b71b7c0a0cc
tdgeter <tdgeter35@gmail.com>
2025-11-02 22:22:39 -0600

    Attempting to work with compose - got images added to the resources, button and image displayed on screen.
    Got class set up for "Dice".
    
    Next will look to start getting logic input for button clicks.

--- Dice Roll/app/src/main/java/com/example/diceroll/MainActivity.kt
+++ Dice Roll/app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroll.ui.theme.DiceRollTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DiceRollTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                )
+                {
+                    Row {
+                        Image(
+                            painter = painterResource(id = R.drawable.dice1),
+                            contentDescription = "Dice Image"
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.dice1),
+                            contentDescription = "Dice Image"
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp ))
+                    Button(onClick = {
+                        rollDice()
+                    }) {
+                        Text(text = "Roll Dice")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+class Dice (val numberOfSides: Int) {
+    fun roll(): Int{
+        return(1..numberOfSides).random()
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiceRollTheme {
-        Greeting("Android")
-    }
+fun rollDice() {
+    val dice1 = Dice(6)
+    val roll = dice1.roll()
 }
