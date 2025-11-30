c46966766338444132d2782dcfb35bf8aae26141
tdgeter <tdgeter35@gmail.com>
2025-11-30 17:03:59 -0600

    Start working on additional formatting.
    My first meaningful change will be to the formatting of the To - Do list that is created as the user adds to the list. Currently show the date it was added, and will work on getting a description text field that can be edited after creation of the item.

--- To Do - Base Build from Abdo/app/src/main/java/com/example/todo_simplified/ui/theme/TodoApp.kt
+++ To Do - Base Build from Abdo/app/src/main/java/com/example/todo_simplified/ui/theme/TodoApp.kt
@@ -2,6 +2,7 @@ package com.example.todo_simplified.ui.theme
 
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -12,7 +13,8 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.example.todo_simplified.data.Task
 import androidx.compose.foundation.lazy.items
-
+import androidx.compose.ui.tooling.preview.Preview
+import java.time.LocalDate.now
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -66,14 +68,25 @@ fun TaskItem(task: Task, onDelete: () -> Unit) {
             .padding(vertical = 4.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)
     ) {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(12.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(task.title)
-            TextButton(onClick = onDelete) { Text("Delete") }
+            Arrangement.SpaceBetween
+        )
+        {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Task:" + task.title)
+                Text("Date Added: " + now().toString())
+                TextButton(onClick = onDelete) { Text("Delete") }
+            }
+
+            Text("Description")
         }
     }
 }
